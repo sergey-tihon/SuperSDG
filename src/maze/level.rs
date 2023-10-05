02ec9266b38602274722796040074c7418bf7814
Sergey Tihon <sergey_tihon@epam.com>
2023-10-05 15:30:54 +0300

    feat: randomly generate initial player position & exit (with chatgpt)

--- src/maze/level.rs
+++ src/maze/level.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 use rand::seq::SliceRandom;
+use std::collections::VecDeque;
 use std::fmt;
 
 pub struct MazeLevelPlugin;
@@ -15,6 +16,8 @@ pub struct MazeLevel {
     pub width: usize,
     pub height: usize,
     pub map: Vec<Vec<char>>,
+    pub start: (usize, usize),
+    pub exit: (usize, usize),
 }
 
 impl MazeLevel {
@@ -26,9 +29,16 @@ impl MazeLevel {
             width,
             height,
             map: vec![vec!['#'; height]; width],
+            start: (0, 0),
+            exit: (0, 0),
         };
 
         maze.generate_maze(1, 1);
+
+        let (start, exit) = maze.random_player_and_exit_positions();
+        maze.start = start;
+        maze.exit = exit;
+
         maze
     }
 
@@ -52,6 +62,49 @@ impl MazeLevel {
                 self.generate_maze(nx, ny);
             }
         }
+    }
+
+    fn random_player_and_exit_positions(&self) -> ((usize, usize), (usize, usize)) {
+        fn bfs(maze: &MazeLevel, start_x: usize, start_y: usize) -> ((usize, usize), usize) {
+            let mut visited = vec![vec![false; maze.height]; maze.width];
+            let mut queue = VecDeque::new();
+            visited[start_x][start_y] = true;
+            queue.push_back((start_x, start_y, 0));
+
+            let mut max_distance = 0;
+            let mut farthest_cell = (start_x, start_y);
+
+            while let Some((x, y, dist)) = queue.pop_front() {
+                for (dx, dy) in [(-1, 0), (1, 0), (0, -1), (0, 1)].iter() {
+                    let nx = (x as i32 + dx) as usize;
+                    let ny = (y as i32 + dy) as usize;
+                    if nx < maze.width
+                        && ny < maze.height
+                        && maze.map[nx][ny] == ' '
+                        && !visited[nx][ny]
+                    {
+                        visited[nx][ny] = true;
+                        queue.push_back((nx, ny, dist + 1));
+                        if dist > max_distance {
+                            max_distance = dist + 1;
+                            farthest_cell = (nx, ny);
+                        }
+                    }
+                }
+            }
+
+            (farthest_cell, max_distance)
+        }
+
+        let mut rng = rand::thread_rng();
+        let empty_cells: Vec<(usize, usize)> = (1..self.width)
+            .flat_map(|x| (1..self.height).map(move |y| (x, y)))
+            .filter(|&(x, y)| self.map[x][y] == ' ')
+            .collect();
+        let &(start_x, start_y) = empty_cells.choose(&mut rng).unwrap();
+        let (exit, _) = bfs(self, start_x, start_y);
+
+        ((start_x, start_y), exit)
     }
 }
 
@@ -82,5 +135,14 @@ mod test {
                 }
             }
         }
+    }
+
+    #[test]
+    fn maze_generation() {
+        let level = MazeLevel::new(20, 20);
+        let (start, exit) = level.random_player_and_exit_positions();
+
+        assert_eq!(level.map[start.0][start.1], ' ');
+        assert_eq!(level.map[exit.0][exit.1], ' ');
     }
 }
