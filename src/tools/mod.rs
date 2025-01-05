d207066e524832d7cf69c353cd8c775344546796
Sergey Tihon <sergey.tihon@gmail.com>
2025-01-05 15:10:40 +0100

    feat: gh deploy workflow

--- src/tools/mod.rs
+++ src/tools/mod.rs
@@ -1,18 +1,23 @@
 use bevy::app::{PluginGroup, PluginGroupBuilder};
 
+#[cfg(feature = "dev")]
 mod debug;
+#[cfg(feature = "dev")]
 mod fps_overlay;
 
 pub struct ToolsPlugins;
 
 impl PluginGroup for ToolsPlugins {
     fn build(self) -> bevy::app::PluginGroupBuilder {
-        if cfg!(feature = "dev") {
+        #[cfg(feature = "dev")]
+        {
             // Plugins for debugging and development
             PluginGroupBuilder::start::<Self>()
                 .add(debug::plugin)
                 .add(fps_overlay::plugin)
-        } else {
+        }
+        #[cfg(not(feature = "dev"))]
+        {
             PluginGroupBuilder::start::<Self>()
         }
     }
